d93a97b23d536485317f0cab7beafbc9642d09d6
Perez Angel <e2238024@cmaisonneuve.qc.ca>
2024-09-05 09:26:55 -0400

    test

--- parties-communes/entete.php
+++ parties-communes/entete.php
@@ -1,3 +1,28 @@
+<?php
+  // test
+  // A) Lire le fichier JSOON contenant les textes
+  // Étape 1 : lire le fichif "i18n/fr.json"
+  // et affecter son conteunu a une varaible PHP
+  $textesJSON = file_get_contents("i18n/fr.json");
+  // Test: 
+  // echo $textes;
+
+  // Étape 2 : convertir le contenu du fichier en variables PHP
+  // pour remettre les textes dans la page Web aux bons endroits
+  $textes = json_decode($textesJSON);
+
+  $_ent = $textes->entete;
+  $_pp = $textes->pp;
+
+  // Test
+  // print_r($textesConvertis);
+  // Imprimer la propriété altLogo de l'objet correspondant à la propriété
+  // entete de cet objet: 
+  // echo $textesConvertis->entete->altLogo;
+  // echo $textesConvertis->entete->placeholderRecherhe;
+?>
+
+
 <!DOCTYPE html>
 <html lang="fr">
 
@@ -24,18 +49,18 @@
       </nav>
       <nav class="barre-logo">
         <label for="cc-btn-responsive" class="material-icons burger">menu</label>
-        <a class="logo" href="index.php"><img src="images/logo.png" alt="Accueil"></a>
+        <a class="logo" href="index.php"><img src="images/logo.png" alt="<?php  echo $_ent->altLogo; ?>"></a>
         <a class="material-icons panier" href="panier.php">shopping_cart</a>
-        <input class="recherche" type="search" name="motscles" placeholder="Recherche">
+        <input class="recherche" type="search" name="motscles" placeholder="<?php  echo $_ent->placeholderRecherhe; ?>">
       </nav>
       <input type="checkbox" id="cc-btn-responsive">
       <nav class="principale">
         <label for="cc-btn-responsive" class="menu-controle material-icons">close</label>
-        <a href="teeshirts.php" class="<?php  if($page=='teeshirts') { echo 'actif';} ?>">Teeshirts</a>
-        <a href="casquettes.php">Casquettes</a>
-        <a href="hoodies.php" class="">Hoodies</a>
+        <a href="teeshirts.php" class="<?php  if($page=='teeshirts') { echo 'actif';} ?>"> <?= $_ent->menuTeeshirts; ?></a>
+        <a href="casquettes.php"><?= $_ent->menuCasquettes; ?></a>
+        <a href="hoodies.php" class=""> <?= $_ent->menuHoodies; ?></a>
         <span class="separateur"></span>
-        <a href="aide.php">Aide</a>
-        <a href="apropos.php">À propos de nous</a>
+        <a href="aide.php"> <?= $_ent->menuAide; ?></a>
+        <a href="apropos.php"> <?= $_ent->menuNous; ?></a>
       </nav>
     </header>
